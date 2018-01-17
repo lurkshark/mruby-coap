eb88a7931cb5e507125a9b1443f80f57a4ee9e1d
lurkshark <joel@dirty.vodka>
2018-01-17 03:56:49 -0800

    Cleanup boilerplate; add travis badge

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mruby-tinydtls
+[![Build Status](https://travis-ci.org/lurkshark/mruby-tinydtls.svg?branch=master)](https://travis-ci.org/lurkshark/mruby-tinydtls)
--- src/mrb_tinydtls.c
+++ src/mrb_tinydtls.c
@@ -8,12 +8,12 @@ mrb_tinydtls_ping(mrb_state *mrb, mrb_value self)
 }
 
 void
-mrb_mruby_tinydtls_gem_init(mrb_state* mrb) {
-    struct RClass *class_cextension = mrb_define_module(mrb, "TinyDTLS");
-    mrb_define_class_method(mrb, class_cextension, "ping", mrb_tinydtls_ping, MRB_ARGS_NONE());
+mrb_mruby_tinydtls_gem_init(mrb_state *mrb) {
+    struct RClass *module_tinydtls = mrb_define_module(mrb, "TinyDTLS");
+    mrb_define_class_method(mrb, module_tinydtls, "ping", mrb_tinydtls_ping, MRB_ARGS_NONE());
 }
 
 void
-mrb_mruby_tinydtls_gem_final(mrb_state* mrb) {
+mrb_mruby_tinydtls_gem_final(mrb_state *mrb) {
     /* finalizer */
 }
